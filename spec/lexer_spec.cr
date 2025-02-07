dfc4ccd3aeffdb22ec244f65a7ed2aba388681dd
Tommy Jeff <threcius@yahoo.com>
2025-02-07 12:44:07 +0800

    Add match_unicode to lexer

--- spec/lexer_spec.cr
+++ spec/lexer_spec.cr
@@ -18,5 +18,42 @@ module CssParser
       s1 = lexer.string_range(0)
       s1.should eq("div")
     end
+
+    it "is white spaces" do
+      lexer = Lexer.new("")
+      lexer.space?(' ').should be_true
+      lexer.space?('\n').should be_true
+      lexer.space?('\r').should be_true
+      lexer.space?('\t').should be_true
+      lexer.space?('\f').should be_true
+      lexer.space?('a').should be_false
+    end
+
+    it "match unicodes" do
+      lexer = Lexer.new("\\01")
+      lexer.match_unicode
+      lexer = Lexer.new("\\01afaf")
+      lexer.match_unicode
+      lexer = Lexer.new("\\01afaf\r\n")
+      lexer.match_unicode
+      lexer = Lexer.new("\\01afaf\n")
+      lexer.match_unicode
+      lexer = Lexer.new("\\01afaf ")
+      lexer.match_unicode
+      lexer = Lexer.new("\\01afaf\t")
+      lexer.match_unicode
+      lexer = Lexer.new("\\01afaf\f")
+      lexer.match_unicode
+
+      expect_raises(Exception, "match unicode macro failed") do
+        lexer = Lexer.new("\\x000")
+        lexer.match_unicode
+      end
+
+      expect_raises(Exception, "match unicode macro failed") do
+        lexer = Lexer.new("abcdef")
+        lexer.match_unicode
+      end
+    end
   end
 end
--- src/lexer.cr
+++ src/lexer.cr
@@ -10,6 +10,60 @@ module CssParser
       @reader = Char::Reader.new(string)
     end
 
+    # Match macro nmstart
+    def match_nmstart
+
+    end
+
+    # Match macro nonascii
+    def match_nonascii
+
+    end
+
+    # Match macro escape
+    def match_escape
+
+    end
+
+    def match_unicode
+      char = current_char
+      if char == '\\'
+        char = next_char
+        char_count = 1
+        while true
+          if char_count > 6
+            break
+          end
+          if (char >= '0' && char <= '9') || (char >= 'a' && char <= 'f')
+            char = next_char
+            char_count += 1
+          else
+            if char_count == 1
+              raise "match unicode macro failed"
+            end
+            break
+          end
+        end
+        if char == '\r'
+          char = next_char
+          if char == '\n'
+            next_char
+          end
+        elsif space?(char)
+          next_char
+        end
+      else
+        raise "match unicode macro failed"
+      end
+    end
+
+    def space?(char : Char)
+      if char.in?(" \n\r\t\f")
+        return true
+      else
+        return false
+      end
+    end
     def next_token
 
     end
