55e2c37d76af4559ca4f518c5c1070e779c5293b
Tommy Jeff <threcius@yahoo.com>
2025-03-03 14:52:13 +0800

    ast: Add DeclarationNode

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,3 @@
 require "spec"
 require "../src/cssparser"
+require "../src/ast"
--- src/ast.cr
+++ src/ast.cr
@@ -1,5 +1,16 @@
+require "./token"
+
 module CssParser
   abstract class ASTNode
 
   end
+
+  class DeclarationNode < ASTNode
+    property ident_token : Token
+    property componet_value : Token
+
+    def initialize(@ident_token, @componet_value)
+    end
+  end
 end
+
